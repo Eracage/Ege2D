221709bb92c74a682e26e356dcaf3acbae5dad2b
Esko Haila <eracage@gmail.com>
2014-04-07 05:48:37 +0300

    Gave this project the name it was meant to have

---
+++ include/Ege2D/PhysicsObject.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef PHYSICSOBJECT_H_EGEPHYSICS
+#define PHYSICSOBJECT_H_EGEPHYSICS
+
+namespace ege
+{
+	class PhysicsObject
+	{
+		PhysicsObject();
+		virtual ~PhysicsObject();
+
+	private:
+	};
+}
+
+#endif //WORLD_H_EGEPHYSICS
